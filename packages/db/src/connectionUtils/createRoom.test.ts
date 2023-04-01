d89f55b7afdc4def28375a164d63a1db8b151e7a
jacobcoro <xiaokourou@gmail.com>
2023-04-01 17:57:08 +0800

    cleanup test =

--- packages/db/src/connectionUtils/createRoom.test.ts
+++ packages/db/src/connectionUtils/createRoom.test.ts
@@ -4,20 +4,16 @@ import { createRoom } from './createRoom';
 
 describe('createRoom', () => {
   it('Can create a room on the matrix server', async () => {
-    try {
-      const { client } = await createRandomMatrixClient();
-      if (!client) throw new Error('No client');
-      const room = await createRoom(client, {
-        roomAliasName: 'test' + (Math.random() * 10000).toFixed(),
-        name: 'Test Room',
-        topic: 'This is a test room',
-      });
-      console.log({ room });
-      expect(room).toHaveProperty('room_id');
-      expect(room.room_id).toMatch(/^!/);
-      expect(room.room_id).toMatch(/:localhost:8888$/);
-    } catch (error) {
-      console.error(error);
-    }
+    const { client } = await createRandomMatrixClient();
+    if (!client) throw new Error('No client');
+    const room = await createRoom(client, {
+      roomAliasName: 'test' + (Math.random() * 10000).toFixed(),
+      name: 'Test Room',
+      topic: 'This is a test room',
+    });
+
+    expect(room).toHaveProperty('room_id');
+    expect(room.room_id).toMatch(/^!/);
+    expect(room.room_id).toMatch(/:localhost:8888$/);
   });
 });
