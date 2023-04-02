1673aeea22f2eb6efbc0d3e8927f95fbcdfea005
jacobcoro <xiaokourou@gmail.com>
2023-04-02 23:23:35 +0800

    hooks example

---
+++ examples/hooks/src/App.test.tsx
@@ -0,0 +1,11 @@
+import { it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+it('Renders hello world', () => {
+  render(<App />);
+  expect(
+    screen.getByRole('heading', {
+      level: 1,
+    })
+  ).toHaveTextContent('Login');
+});
---
+++ examples/hooks/vite.config.ts
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
+  },
+});
