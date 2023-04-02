0913ded1a41fac5bc1cb0ec76f9209377619fe93
jacobcoro <xiaokourou@gmail.com>
2023-04-02 22:43:22 +0800

    update packages

--- examples/basic/src/App.test.tsx
+++ examples/basic/src/App.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it } from 'vitest';
+import { it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 it('Renders hello world', () => {
