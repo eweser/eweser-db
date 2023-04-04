9974967e165218a5d68a1b6900ebc8b95fcd3d95
jacobcoro <xiaokourou@gmail.com>
2023-04-04 23:41:29 +0800

    remove paths

--- packages/example-basic/vite.config.ts
+++ packages/example-basic/vite.config.ts
@@ -2,11 +2,10 @@
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
 
   build: {
     minify: false,
