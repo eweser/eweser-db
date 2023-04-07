94faecf6f75bb75340cf56dceda536855f3b0aa3
jacobcoro <xiaokourou@gmail.com>
2023-04-07 19:54:00 +0800

    get rid of hooks

--- packages/hooks/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as useLogin } from './useLogin';
-export { default as useCollection } from './useCollection';
-export { DatabaseContext, DatabaseProvider } from './DatabaseProvider';
-export { CollectionContext, CollectionProvider } from './CollectionProvider';
