ea865e80485d812f57fd081baf0bb64072eb3a82
jacobcoro <xiaokourou@gmail.com>
2023-04-14 23:18:49 +0800

    rename the apps for clarity

--- packages/example-editor/src/main.tsx
+++ packages/example-editor/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './AppEditor';
 import '@milkdown/theme-nord/style.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
