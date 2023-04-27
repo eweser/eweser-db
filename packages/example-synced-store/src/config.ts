488d39c937907077c7707cd8552af3a0e3c205bd
jacobcoro <xiaokourou@gmail.com>
2023-04-27 10:10:35 +0800

    be explicit about showing signup button

--- packages/example-synced-store/src/config.ts
+++ packages/example-synced-store/src/config.ts
@@ -4,16 +4,18 @@ const dummyUserPass = 'dumdum';
 export const MATRIX_SERVER =
   import.meta.env.VITE_MATRIX_SERVER ?? 'https://matrix.org';
 
-const env =
+export const env =
   import.meta.env.VITE_CI === 'true'
     ? 'ci'
     : import.meta.env.DEV
     ? 'dev'
     : 'prod';
 
-export const dev = import.meta.env.DEV;
+export const dev = env === 'dev';
+
+export const showSignup = env !== 'prod';
 
 export const DEV_USERNAME =
-  import.meta.env.VITE_DEV_USERNAME ?? env !== 'prod' ? dummyUserName : '';
+  import.meta.env.VITE_DEV_USERNAME ?? dev ? dummyUserName : '';
 export const DEV_PASSWORD =
-  import.meta.env.VITE_DEV_PASSWORD ?? env !== 'prod' ? dummyUserPass : '';
+  import.meta.env.VITE_DEV_PASSWORD ?? dev ? dummyUserPass : '';
