4d08fca4857534ecf7e80561d61e43bf750df28d
jacobcoro <xiaokourou@gmail.com>
2023-06-08 00:01:06 +0800

    explain max concurrency

--- packages/db/vite.config.js
+++ packages/db/vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     setupFiles: 'src/setupTests.ts',
+    // need to slow down the tests or the server will reject the requests
     maxConcurrency: 2,
     maxThreads: 2,
     minThreads: 1,
