2f69c3e4e7f3e8c3b1f6b86d866a2a535554fd0f
jacobcoro <xiaokourou@gmail.com>
2024-02-11 17:39:48 +0800

    add terms of service

---
+++ packages/auth-server/src/app/statement/privacy/page.tsx
@@ -0,0 +1,61 @@
+import { siteConfig } from '@/config/site';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: siteConfig.pageName('Privacy'),
+};
+
+import React from 'react';
+
+const PrivacyStatement = () => (
+  <div className="max-w-xl self-center space-y-4 p-10">
+    <h2 className="text-xl font-bold">Privacy Statement for eweser.com</h2>
+
+    <p>
+      <strong>
+        At eweser.com, we are committed to protecting the privacy of our users.
+      </strong>{' '}
+      {`Our services allow users to manage their own user-owned databases, with options for hosting through eweser.com or choosing their own authentication server provider. Here's how we handle your data:`}
+    </p>
+
+    <p>
+      <strong>Information We Collect:</strong> We collect information necessary
+      to provide our services, such as your email address and any data you store
+      in your database. We may also collect usage data to improve the
+      functionality of eweser.com.
+    </p>
+
+    <p>
+      <strong>How We Use Your Data:</strong> Your information helps us deliver
+      and improve our services, provide customer support, and ensure the
+      security of our platform. We do not share your personal data with
+      advertisers or third parties for marketing purposes.
+    </p>
+
+    <p>
+      <strong>Data Hosting and Security:</strong> You have the choice to host
+      your database with us or select an external provider. We strive to protect
+      your data but do not guarantee that data loss will never occur.
+    </p>
+
+    <p>
+      <strong>Your Rights:</strong> You have the right to access, modify, and
+      delete your data. If you wish to exercise these rights or have any
+      questions, please contact us directly.
+    </p>
+
+    <p>
+      <strong>Changes to This Statement:</strong> We may update our Privacy
+      Statement from time to time. We encourage users to review it regularly to
+      stay informed about how we are protecting your information.
+    </p>
+
+    <p>
+      This statement is a summary of our full Privacy Policy. For more detailed
+      information, please read our complete policy or contact us if you have any
+      questions.
+    </p>
+  </div>
+);
+
+export default PrivacyStatement;
---
+++ packages/auth-server/src/app/statement/terms-of-service/page.tsx
@@ -0,0 +1,72 @@
+import { siteConfig } from '@/config/site';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: siteConfig.pageName('Privacy'),
+};
+
+import React from 'react';
+
+const TermsOfService = () => (
+  <div className="max-w-xl self-center space-y-4 p-10">
+    <h2 className="text-xl font-bold">
+      Terms of Service Summary for eweser.com
+    </h2>
+
+    <p>
+      <strong>Account Creation and Use:</strong> You must provide accurate
+      information when creating an account. You are responsible for maintaining
+      the security of your account and for all activities that occur under your
+      account.
+    </p>
+
+    <p>
+      <strong>User Data:</strong> You retain ownership of any data you store on
+      eweser.com. However, by using our service, you grant us the permission to
+      host, handle, and back up your data as necessary to provide and maintain
+      our services.
+    </p>
+
+    <p>
+      <strong>Service Availability:</strong> While we aim to provide a reliable
+      and uninterrupted service, we cannot guarantee that the service will
+      always be available or free from errors.
+    </p>
+
+    <p>
+      <strong>Changes to Services:</strong> Our basic service is currently free,
+      but this may change in the future. We may also offer additional, paid
+      services, such as enhanced backup options.
+    </p>
+
+    <p>
+      <strong>Liability:</strong> We are not liable for any data loss or
+      corruption. We recommend regularly backing up your data to prevent loss.
+    </p>
+
+    <p>
+      <strong>Amendments to Terms:</strong> We may update these terms from time
+      to time. We will notify users of any significant changes, but we encourage
+      you to review the ToS periodically.
+    </p>
+
+    <p>
+      <strong>Governing Law:</strong> Any disputes related to these terms will
+      be governed by the laws of the jurisdiction in which our company is
+      registered.
+    </p>
+
+    <p>
+      <strong>Contact Us:</strong> If you have any questions or concerns about
+      these terms, please contact us.
+    </p>
+
+    <h2 className="text-xl font-bold">Acknowledgment</h2>
+    <p>
+      By using eweser.com, you acknowledge that you have read, understood, and
+      agree to be bound by these Terms of Service.
+    </p>
+  </div>
+);
+
+export default TermsOfService;
