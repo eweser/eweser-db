1ced0665000d4216028e91aa12f9af1f01b6f2bc
jacobcoro <xiaokourou@gmail.com>
2024-02-12 18:46:30 +0800

    use codeblock for error

--- packages/auth-server/src/app/error/page.tsx
+++ packages/auth-server/src/app/error/page.tsx
@@ -10,7 +10,7 @@ export default function ErrorPage({
   return (
     <div className="max-w-lg self-center pt-10">
       <h1 className="text-2xl font-bold mb-4">Error</h1>
-      <p className="py-2">{message || 'An error occurred.'}</p>
+      <code className="py-2">{message || 'An error occurred.'}</code>
 
       <Link
         href="/"
