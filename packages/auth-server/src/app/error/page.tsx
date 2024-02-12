1bd744e407190b30ebce657afae5e4cf2d98c8ec
jacobcoro <xiaokourou@gmail.com>
2024-02-12 18:49:43 +0800

    style error box

--- packages/auth-server/src/app/error/page.tsx
+++ packages/auth-server/src/app/error/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/button';
 import Link from 'next/link';
 
 export default function ErrorPage({
@@ -10,14 +11,12 @@ export default function ErrorPage({
   return (
     <div className="max-w-lg self-center pt-10">
       <h1 className="text-2xl font-bold mb-4">Error</h1>
-      <code className="py-2">{message || 'An error occurred.'}</code>
-
-      <Link
-        href="/"
-        className="underline underline-offset-4 hover:text-secondary/90 text-secondary "
-      >
-        Go back home{' '}
-      </Link>
+      <hr className="my-5" />
+      <code>{message || 'An error occurred.'}</code>
+      <hr className="my-5" />
+      <Button asChild>
+        <Link href="/">Go back home</Link>
+      </Button>
     </div>
   );
 }
