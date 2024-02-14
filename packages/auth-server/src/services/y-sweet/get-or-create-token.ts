12e88104815569efec3fbb9f51824c12f2b0152d
jacobcoro <xiaokourou@gmail.com>
2024-02-15 01:09:04 +0800

    trycatch get or create token

--- packages/auth-server/src/services/y-sweet/get-or-create-token.ts
+++ packages/auth-server/src/services/y-sweet/get-or-create-token.ts
@@ -1,6 +1,13 @@
 import { getOrCreateDocAndToken } from '@y-sweet/sdk';
 import { Y_SWEET_CONNECTION_STRING } from './config';
+import { logger } from '@/shared/utils';
 
 export async function getOrCreateToken(roomId: string) {
-  return await getOrCreateDocAndToken(Y_SWEET_CONNECTION_STRING, roomId);
+  try {
+    return await getOrCreateDocAndToken(Y_SWEET_CONNECTION_STRING, roomId);
+  } catch (error) {
+    logger(error);
+    // hide the error because it exposes y-sweet url
+    throw new Error('Failed to get or create doc token');
+  }
 }
