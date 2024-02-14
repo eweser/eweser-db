364b5fddaa30c7109b1296f45e092bd18619912a
jacobcoro <xiaokourou@gmail.com>
2024-02-14 21:11:36 +0800

    auth server url env

--- packages/auth-server/src/shared/constants.ts
+++ packages/auth-server/src/shared/constants.ts
@@ -5,3 +5,9 @@
 export const COLLECTION_KEYS = ['notes', 'flashcards', 'profiles'] as const;
 
 export const REQUESTER_TYPES = ['app', 'user'] as const;
+
+export const AUTH_SERVER_URL = process.env.NEXT_PUBLIC_AUTH_SERVER_URL ?? '';
+
+if (!AUTH_SERVER_URL) {
+  throw new Error('AUTH_SERVER_URL is not defined');
+}
