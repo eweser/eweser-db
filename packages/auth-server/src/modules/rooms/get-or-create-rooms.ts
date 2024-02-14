f64aff336f8167aaf6531d5cd677a7045bd7919a
jacobcoro <xiaokourou@gmail.com>
2024-02-14 21:13:06 +0800

    getOrCreateRoom placeholder

---
+++ packages/auth-server/src/modules/rooms/get-or-create-rooms.ts
@@ -0,0 +1,11 @@
+/**
+ * check database for room record.
+ * return room token if found
+ * create room if not found
+ * get room token from y-sweet if token is not found in room
+ * save room token to database
+ * return room token
+ */
+export async function getOrCreateRoom() {
+  // const existingRoom = await getRoomsByUserId(userId);
+}
