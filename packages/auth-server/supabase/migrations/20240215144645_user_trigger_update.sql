12e98112042818efa26fe0c49e9de667ec14e679
jacobcoro <xiaokourou@gmail.com>
2024-02-15 19:59:18 +0800

    update new user trigger

---
+++ packages/auth-server/supabase/migrations/20240215144645_user_trigger_update.sql
@@ -0,0 +1,15 @@
+create
+or replace function public.handle_new_user () returns trigger as $$
+begin
+  insert into public.users(id, email)
+  values (new.id, new.email);
+  return new;
+end;
+$$ language plpgsql security definer;
+
+create
+or replace trigger on_auth_user_created
+after
+  insert on auth.users for each row
+execute
+  procedure public.handle_new_user ();
