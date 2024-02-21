e05b5f8e4271b18a354f2fe4ccae3c20933c3e8c
jacobcoro <xiaokourou@gmail.com>
2024-02-22 00:39:35 +0800

    access token sync

---
+++ packages/auth-server/src/app/access-grant/sync-registry/route.ts
@@ -0,0 +1,24 @@
+import { syncRoomsWithClient } from '@/modules/rooms/sync-rooms-with-client';
+import { authTokenFromHeaders, serverRouteError } from '@/shared/utils';
+import type {
+  RegistrySyncRequestBody,
+  RegistrySyncResponse,
+} from '@eweser/shared';
+
+export default async function POST(request: Request) {
+  const token = authTokenFromHeaders(request.headers);
+  if (!token) {
+    return serverRouteError('No token provided', 401);
+  }
+
+  // TODO: add validation
+  const { rooms } = (await request.json()) as RegistrySyncRequestBody;
+
+  const { rooms: newRooms, token: newToken } = await syncRoomsWithClient(
+    token,
+    rooms
+  );
+  const response: RegistrySyncResponse = { rooms: newRooms, token: newToken };
+
+  return Response.json(response);
+}
