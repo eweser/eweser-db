e6f5dd4aa99f98a05d75b6e613e35a76e5706e03
jacobcoro <xiaokourou@gmail.com>
2024-03-01 21:16:16 +0800

    update seed

--- packages/auth-server/supabase/seed.sql
+++ packages/auth-server/supabase/seed.sql
@@ -1,1 +1,2 @@
-INSERT INTO config (key, value) VALUES ('auth_server_url', 'https://eweser.com');
+INSERT INTO apps (id, domain) VALUES (gen_random_uuid(), 'eweser.com');
+INSERT INTO apps (id, domain) VALUES (gen_random_uuid(), 'eweser-db-example-basic.netlify.app');
