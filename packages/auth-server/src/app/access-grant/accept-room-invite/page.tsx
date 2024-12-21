7992b633d70d91f6dbe1f01aef5a4db54864893b
jacobcoro <xiaokourou@gmail.com>
2024-12-21 20:33:43 +0800

    Revert "next upgrade"
    This reverts commit 235970f2944cacf01c100f3f5b2098b141d42f8e.

--- packages/auth-server/src/app/access-grant/accept-room-invite/page.tsx
+++ packages/auth-server/src/app/access-grant/accept-room-invite/page.tsx
@@ -16,13 +16,12 @@ import { AcceptingInviteLoading } from '../../../frontend/components/access-gran
 export default async function AcceptRoomInvitePage({
   searchParams,
 }: {
-  searchParams: Promise<AcceptRoomInviteQueries>;
+  searchParams: AcceptRoomInviteQueries;
 }) {
   let error = '';
   let options: CreateRoomInviteBody | undefined;
   let acceptRoomInviteParams: AcceptRoomInviteParams | undefined;
-
-  const token = (await searchParams)?.token;
+  const token = searchParams?.token;
   if (!token) {
     error = 'No token provided';
   } else {
