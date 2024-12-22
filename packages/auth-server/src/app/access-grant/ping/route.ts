5fdf73fd35c9908f36eb3efb3a2325297321625e
jacobcoro <xiaokourou@gmail.com>
2024-12-22 15:53:38 +0800

    addcors right to options request?

--- packages/auth-server/src/app/access-grant/ping/route.ts
+++ packages/auth-server/src/app/access-grant/ping/route.ts
@@ -3,5 +3,12 @@ export async function GET() {
 }
 
 export function OPTIONS() {
-  return new Response('ok', { status: 200 });
+  return new Response('ok', {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  });
 }
