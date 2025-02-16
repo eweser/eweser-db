4783801b81aa67d67387d217412ebeab308e9544
jacobcoro <xiaokourou@gmail.com>
2025-02-16 21:40:41 +0800

    update eslint

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -1,9 +1,22 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+
+export default tseslint.config({
+  extends: [eslint.configs.recommended, tseslint.configs.strict],
+
+  languageOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2022,
+    globals: {
+      ...globals.browser,
+      ...globals.node,
+    },
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  plugins: {
+    prettier,
+  },
   rules: {
     'prettier/prettier': [
       'warn',
@@ -17,29 +30,13 @@ module.exports = {
     'linebreak-style': 'off', // ignore overly strict linebreak style rule
     'prefer-const': 'warn', // Warn about using const instead of let. Helps to avoid accidental reassignment
     'no-console': 'warn',
-    'no-unused-vars': [
+    '@typescript-eslint/consistent-type-imports': 'warn', // Require consistent use of type imports
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-vars': 'off', // typescript-eslint/no-unused-vars will cover this, and wont throw errors for unused variables in function parameters for ts types
+    '@typescript-eslint/no-unused-vars': [
       'warn',
       { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-    ],
+    ], // Warn about unused variables to avoid leaving them in the code accidentally and creating clutter.
+    '@typescript-eslint/ban-ts-comment': 'off', // Allow ts-ignore comments to turn off type checking when needed
   },
-  overrides: [
-    {
-      files: ['*.ts'],
-      extends: ['plugin:@typescript-eslint/recommended'],
-      parser: '@typescript-eslint/parser',
-
-      plugins: ['@typescript-eslint'],
-      rules: {
-        'prefer-const': 'warn',
-        '@typescript-eslint/consistent-type-imports': 'warn', // Require consistent use of type imports
-        '@typescript-eslint/no-explicit-any': 'off',
-        'no-unused-vars': 'off', // typescript-eslint/no-unused-vars will cover this, and wont throw errors for unused variables in function parameters for ts types
-        '@typescript-eslint/no-unused-vars': [
-          'warn',
-          { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-        ], // Warn about unused variables to avoid leaving them in the code accidentally and creating clutter.
-        '@typescript-eslint/ban-ts-comment': 'off', // Allow ts-ignore comments to turn off type checking when needed
-      },
-    },
-  ],
-};
+});
