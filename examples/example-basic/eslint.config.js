c8c5d5050a7d380dc1ccfb40081e7ed53d9708fb
jacobcoro <xiaokourou@gmail.com>
2025-02-17 11:55:30 +0800

    update lint

--- examples/example-basic/eslint.config.js
+++ examples/example-basic/eslint.config.js
@@ -3,7 +3,7 @@ import eweserBase from '@eweser/eslint-config-react-ts';
 export default [
   ...eweserBase,
   {
-    files: ['./src/**/*.ts', 'types/**/*.ts'],
+    files: ['./src/'],
   },
   {
     ignores: [
